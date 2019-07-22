81089a5b48eea8ac75e4161151014dcf93de5046
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-07-22 14:03:04 +0200

    Adding first and skip to post

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -39,6 +39,8 @@ const Query = {
     },
     posts(parent, args, { prisma }, info) {
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
             where: {
                 published: true
             }
